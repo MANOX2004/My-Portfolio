7ad537178b632972d7c5b971fd1eb725e9481a46
MANOX2004 <manojsenevirathnabamunuge@gmail.com>
2026-08-09 12:20:20 +0530

    change contact me decription

--- src/portfolio.js
+++ src/portfolio.js
@@ -728,7 +728,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "animated_ashutosh.png",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on almost every social media. You can message me, I will reply within 24 hours. I'm helping you as much as I can, using my best knowledge.",
   },
   blogSection: {
     title: "Blogs",
